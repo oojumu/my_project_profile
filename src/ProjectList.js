e6592deaca41a750352f142e4090bda679133a57
oojum <oojumu@yahoo.com>
2024-08-22 20:09:27 +0100

    {adding another section to the Project section}

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -13,7 +13,11 @@ console.log(loading, projects);
             <div className='title'>
                 <h2>Project Portfolio</h2>
             </div>
-
+            <div className="d-grid gap-3">
+                <div className="p-2 bg-light border">Grid item 1</div>
+                <div className="p-2 bg-light border">Grid item 2</div>
+                <div className="p-2 bg-light border">Grid item 3</div>
+            </div>
             <div className='title-underline'>
                 
             </div>

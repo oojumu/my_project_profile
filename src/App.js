7bdea9d8f9441d702531e02ca0c4b5d3ef35172b
oojumu <90765132+oojumu@users.noreply.github.com>
2025-04-15 10:27:19 +0100

    pushing updated portfolio Template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-//import logo from './logo.svg';
-//import './App.css';
+
+import Global from './components/Global';
 import ProfileDesc from './ProfileDesc';
 import ProjectList from './ProjectList';
+import './assets/css/bootstrap.min.css'
+import './css/App.css'
+import './css/responsive.css'
+//import Portfolio from './components/Main-Components/Portfolio';
+
 
 function App() {
     return   (
-              <main>
-                <ProfileDesc />
-                <ProjectList />
+              <main>   
+                <Global/>
+                  <ProfileDesc/>
+                  <ProjectList/>    *
+                       
               </main>        
     );
 
